063e659505a2c198bc46ee33c0c894c45e427f63
Dragon <dragon@MacBook-de-Dragon.local>
2019-08-01 17:58:33 -0700

    VC in storyboard

---
+++ TestAppleRobertoG/TestAppleRobertoG/LogInViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LogInViewController.swift
+//  TestAppleRobertoG
+//
+//  Created by Dragon on 01/08/19.
+//  Copyright © 2019 Roberto Gonzalez C. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TestAppleRobertoG/TestAppleRobertoG/MoviesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MoviesTableViewCell.swift
+//  TestAppleRobertoG
+//
+//  Created by Dragon on 01/08/19.
+//  Copyright © 2019 Roberto Gonzalez C. All rights reserved.
+//
+
+import UIKit
+
+class MoviesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
