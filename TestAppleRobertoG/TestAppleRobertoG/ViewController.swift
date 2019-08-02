a35902d0c2b7c3c0b0d46e1f7b9246dfdc3411be
Dragon <dragon@MacBook-de-Dragon.local>
2019-08-01 17:50:10 -0700

    IOS test

---
+++ TestAppleRobertoG/TestAppleRobertoG/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestAppleRobertoG
+//
+//  Created by Dragon on 01/08/19.
+//  Copyright © 2019 Roberto Gonzalez C. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
